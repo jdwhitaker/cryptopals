717526bdee3ead17133280fe2542e1ad4716940e
Justin Whitaker <justin.whitaker@meraki.net>
2024-04-09 09:29:44 -0400

    test padding edge case

--- solutions/set_2/challenge_15/test_solution.py
+++ solutions/set_2/challenge_15/test_solution.py
@@ -1,5 +1,6 @@
 import pytest
 from .solution import pkcs7_unpad
+from ...set_2.challenge_9.solution import pkcs7_padding
 
 def test_solution1():
     input = b"ICE ICE BABY\x04\x04\x04\x04"
@@ -16,3 +17,12 @@ def test_solution3():
     with pytest.raises(Exception):
         input = b"ICE ICE BABY\x01\x02\x03\x04"
         pkcs7_unpad(input)
+
+def test_solution4():
+    input = b"AAAAAAAAAAAAAAA\x01"
+    print(input)
+    output = pkcs7_padding(input, 16)
+    print(output)
+    output = pkcs7_unpad(output)
+    print(output)
+    assert output == input
--- solutions/set_2/challenge_9/test_solution.py
+++ solutions/set_2/challenge_9/test_solution.py
@@ -9,3 +9,8 @@ def test_pkcs_padding2():
     input = b"YELLOW SUBMARINE"
     output = pkcs7_padding(input, 4)
     assert output == b"YELLOW SUBMARINE\x04\x04\x04\x04"
+
+def test_pkcs_padding3():
+    input = b"AAAAAAAAAAAAAAA\x01"
+    output = pkcs7_padding(input, 16)
+    assert output == (input + b"\x10" * 16)
