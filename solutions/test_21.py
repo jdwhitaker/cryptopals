e7df47e8e8fcbbae3991a563b897639c27075ade
Justin Whitaker <juwhitak@cisco.com>
2024-11-15 12:44:01 -0500

    more thorough tests

--- solutions/test_21.py
+++ solutions/test_21.py
@@ -2,5 +2,6 @@ import cryptopals
 
 def test_21():
     rng = cryptopals.MersenneTwisterRNG(1234)
-    r = rng.random()
-    assert r == 822569775 # from https://leventozturk.com/engineering/random/
+    assert rng.random() == 822569775 # from https://leventozturk.com/engineering/random/
+    assert rng.random() == 2137449171
+    assert rng.random() == 2671936806
